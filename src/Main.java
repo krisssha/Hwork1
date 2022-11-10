d03e00ad3c064759072c2e972f8497fd93cb7106
Кристина <duleba@eml.ru>
2022-11-10 16:37:09 +0700

    первые задания

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
     public static void main(String[] args) {
 
+
+        for (int i = 1; i<=10; i++)
+
+            System.out.println(i);
+        System.out.println();
+
+        for (int a = 10; a >= 1; a--)
+
+            System.out.println(a);
+        System.out.println();
+
+        for (int b = 0; b <= 17; b += 2)
+
+        System.out.println(b);
+        System.out.println();
+
+        for (int g = 1904; g <= 2096; g += 4)
+            System.out.println(g + " год является високосным! ");
+        System.out.println();
+
+        for (int s = 7; s <= 98 ; s += 7)
+            System.out.println(s);
+        System.out.println();
+
+        System.out.println(1);
+        for (int d = 2; d <= 512; d *= 2)
+            System.out.println(d);
     }
 }
